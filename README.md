441ee4ec596c51cd53c3e08ba0294f3aa0147c41
Eddyfabbricatore <eddyfabbricatore2@gmail.com>
2023-12-29 10:58:31 +0100

    [edit] layout

--- README.md
+++ README.md
@@ -1,8 +1,5 @@
 # js-our-team
 
-===
-
 # Creare un array di oggetti
-# Ogni oggetto avrà dei valori: nome, ruolo e 'foto';
-# Stampare i valori in console
+# Ogni oggetto avrà dei valori: nome, ruolo e immagine;
 # Stampare gli stessi valori in pagina
--- script.js
+++ script.js
@@ -1,80 +1,56 @@
 const team = [
   {
-    nome: 'Wayne Barnett',
-    ruolo: 'Founder & CEO',	
-    foto: 'wayne-barnett-founder-ceo.jpg'
+    name: 'Wayne Barnett',
+    role: 'Founder & CEO',	
+    img: 'wayne-barnett-founder-ceo.jpg'
   },
 
   {
-    nome: 'Angela Caroll',
-    ruolo: 'Chief Editor',	
-    foto: 'angela-caroll-chief-editor.jpg'
+    name: 'Angela Caroll',
+    role: 'Chief Editor',	
+    img: 'angela-caroll-chief-editor.jpg'
   },
 
   {
-    nome: 'Walter Gordon',
-    ruolo: 'Office Manager',	
-    foto: 'walter-gordon-office-manager.jpg'
+    name: 'Walter Gordon',
+    role: 'Office Manager',	
+    img: 'walter-gordon-office-manager.jpg'
   },
 
   {
-    nome: 'Angela Lopez',
-    ruolo: 'Social Media Manager',	
-    foto: 'angela-lopez-social-media-manager.jpg'
+    name: 'Angela Lopez',
+    role: 'Social Media Manager',	
+    img: 'angela-lopez-social-media-manager.jpg'
   },
 
   {
-    nome: 'Scott Estrada',
-    ruolo: 'Developer',	
-    foto: 'scott-estrada-developer.jpg'
+    name: 'Scott Estrada',
+    role: 'Developer',	
+    img: 'scott-estrada-developer.jpg'
   },
 
   {
-    nome: 'Barbara Ramos',
-    ruolo: 'Graphic Designer',	
-    foto: 'barbara-ramos-graphic-designer.jpg'
+    name: 'Barbara Ramos',
+    role: 'Graphic Designer',	
+    img: 'barbara-ramos-graphic-designer.jpg'
   }
 ];
 
 const row = document.querySelector('.row');
 
-for(let membro of team){
-
-  let nome = '';
-  
-  for(let membroNome of membro.nome){
-      nome += `<li>${membroNome}: ${membro.nome[membroNome]}</li>`;
-      console.log(membro.nome);
-  }
-
-  let ruolo = '';
-  
-  for(let membroRuolo of membro.ruolo){
-    ruolo += `<li>${membroRuolo}: ${membro.ruolo[membroRuolo]}</li>`;
-    console.log(membro.ruolo);
-  }
-
-  let foto = '';
-  
-  for(let membroFoto of membro.foto){
-    foto += `<li>${membroFoto}: ${membro.foto[membroFoto]}</li>`;
-    console.log(membro.foto);
-  }
-
+for(let member of team){
   row.innerHTML += `
-  <div class="col my-3 p-3" style="flex: auto;">
-    <div class="card w-100" style="border: none;">
-      <div class="card-top w-100">
-        <img src="img/${membro.foto}" alt="Ciao"> <br>
+  <div class="col">
+    <div class="card">
+      <div class="card-top">
+        <img src="img/${member.img}" alt="${member.name}">
       </div>
 
-      <div class="card bottom" style="border: none;">
-        <h6 class="mt-3">${membro.nome}</h6> <br>
-        <h6 class="mb-3">${membro.ruolo}</h6> <br>
+      <div class="card-bottom d-flex flex-column justify-content-center align-items-center border-0 p-3">
+        <h6>${member.name}</h6>
+        <h6>${member.role}</h6>
       </div>
     </div>
   </div>
   `;
-
-  console.log('-----------');
 }
